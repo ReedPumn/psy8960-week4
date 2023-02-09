2ac96d9bf0273dc8f36c28beb7d04587c37eaa2a
Reed W Priest <pries153@umn.edu>
2023-02-09 17:52:16 -0600

    Steps 1:13 done.

--- R/week4.R
+++ R/week4.R
@@ -7,3 +7,6 @@ import_tbl <- read_delim(file = "../data/week4.dat", delim = "-", col_names = c(
 glimpse(import_tbl)
 wide_tbl <- import_tbl %>% separate(col = qs, into = c("q1", "q2", "q3", "q4", "q5"), sep = " - ")
 wide_tbl[5:9] <- sapply(wide_tbl[5:9], as.integer)
+wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%b %d %Y, %H:%M:%S")
+wide_tbl[5:9] <- na_if(wide_tbl[5:9], 0)
+wide_tbl <- wide_tbl[wide_tbl$q2 != "NA",]
