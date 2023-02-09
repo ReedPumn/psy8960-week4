08831cae287cf2cd613f8b3bef5e9c6057228cda
Reed W Priest <pries153@umn.edu>
2023-02-09 14:38:16 -0600

    Steps 1:9 done.

--- R/week4.R
+++ R/week4.R
@@ -4,7 +4,5 @@ library(tidyverse)
 
 # Data Import
 import_tbl <- read_delim(file = "../data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
-                         
-                                                                   
-                                                               
-                                                               
+glimpse(import_tbl)
+wide_tbl <- import_tbl %>% separate(col = qs, into = c("q1", "q2", "q3", "q4", "q5"), sep = " - ", convert = TRUE)
